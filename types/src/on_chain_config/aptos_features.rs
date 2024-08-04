8db31e122f8ac20d730dcc1b3c5776d81d3c97c0
George Mitenkov <georgemitenk0v@gmail.com>
2024-08-04 17:20:30 +0100

    [move][aptos-vm] Introduce adapters, test module storage and e2e AptosVM integration
    - TestModuleStorage and TestScriptStorage replace DummyCodeStorage
      in most of the tests.
    - AptosModuleStorage and AptosScriptStorage templates added. The
      types are propagated throughout AptosVM almost fully.
    - Adapter to treat StateView as AptosModuleStorage or AptosScriptStorage
      is added, and propagated to tests and other users instead of
      DummyCodeStorage.
    - Documentation is improved.
    - Verification APIs are added and publishing APIs are removed from MoveVM,
      and tests are changed accordingly.
    - Loader V2 integrated into MoveVM and AptosVM to run e2e tests
      outside of block context, skipping verification for now.

--- types/src/on_chain_config/aptos_features.rs
+++ types/src/on_chain_config/aptos_features.rs
@@ -10,8 +10,10 @@ use move_core_types::{
     effects::{ChangeSet, Op},
     language_storage::CORE_CODE_ADDRESS,
 };
+use move_vm_runtime::should_use_loader_v2;
 use serde::{Deserialize, Serialize};
 use strum_macros::{EnumString, FromRepr};
+
 /// The feature flags define in the Move source. This must stay aligned with the constants there.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, FromRepr, EnumString)]
 #[allow(non_camel_case_types)]
@@ -92,6 +94,7 @@ pub enum FeatureFlag {
     ENABLE_ENUM_TYPES = 74,
     ENABLE_RESOURCE_ACCESS_CONTROL = 75,
     REJECT_UNSTABLE_BYTECODE_FOR_SCRIPT = 76,
+    USE_LOADER_V2 = 77,
 }
 
 impl FeatureFlag {
@@ -167,6 +170,9 @@ impl FeatureFlag {
             FeatureFlag::ENABLE_ENUM_TYPES,
             FeatureFlag::ENABLE_RESOURCE_ACCESS_CONTROL,
             FeatureFlag::REJECT_UNSTABLE_BYTECODE_FOR_SCRIPT,
+            // TODO(loader_v2): Enable the feature flag once ready, for now the feature is
+            //                  enabled using an environment variable.
+            // FeatureFlag::USE_LOADER_V2,
         ]
     }
 }
@@ -186,6 +192,10 @@ impl Default for Features {
 
         for feature in FeatureFlag::default_features() {
             features.enable(feature);
+        }
+
+        if should_use_loader_v2() {
+            features.enable(FeatureFlag::USE_LOADER_V2);
         }
 
         features
@@ -302,6 +312,10 @@ impl Features {
 
     pub fn is_abort_if_multisig_payload_mismatch_enabled(&self) -> bool {
         self.is_enabled(FeatureFlag::ABORT_IF_MULTISIG_PAYLOAD_MISMATCH)
+    }
+
+    pub fn use_loader_v2(&self) -> bool {
+        self.is_enabled(FeatureFlag::USE_LOADER_V2)
     }
 
     pub fn get_max_identifier_size(&self) -> u64 {
